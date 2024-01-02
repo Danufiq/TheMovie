eed1525fda787221cc6159b9de94d3b031a52bca
dana nur fiqi <danufiq98@gmail.com>
2024-01-02 14:52:21 +0700

    testt commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // You can add or override rules here
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'revert']],
+    'scope-enum': [2, 'always', ['example', 'feature', 'bugfix']],
+    // Customize your rules according to your needs
+  },
 };
 
